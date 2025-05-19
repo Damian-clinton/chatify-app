bb01eb212c164abebca8f873e1c6527ba0f3d005
Damian-clinton <kyleclinton54@gmail.com>
2025-05-19 16:07:19 +0100

    Update App.jsx

--- chatify/src/App.jsx
+++ chatify/src/App.jsx
@@ -1,23 +1,32 @@
-import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
-import React, {useState, useEffect} from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import Chattify from "./components/Chattify.jsx";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 
-function App( ) {
- const [isLoggedIn, setIsLoggedIn] = useState(null);
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   const [hasConversation, setHasConversation] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setIsLoggedIn(window.localStorage.getItem('user'));
-    setHasConversation(window.sessionStorage.getItem('selectedConversationID'));
+    setIsLoggedIn(window.localStorage.getItem("user"));
+    setHasConversation(window.sessionStorage.getItem("selectedConversationID"));
+    setLoading(false);
   }, []);
 
+  if (loading) return <div>Loading...</div>;
+
   return (
-    <Router >
+    <Router>
       <Routes>
-        <Route path="/login" element={ <Login />} />
-        <Route path="/" element={ !isLoggedIn && !hasConversation? <Navigate to ="/login" /> : <Chattify /> } />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            !isLoggedIn && !hasConversation ? <Navigate to="/login" /> : <Chattify />
+          }
+        />
         <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
@@ -25,3 +34,4 @@ function App( ) {
 }
 
 export default App;
+
