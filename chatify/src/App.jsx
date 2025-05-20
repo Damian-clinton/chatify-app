22d03e43392354707a01e78088aca939dcc835cc
Damian-clinton <kyleclinton54@gmail.com>
2025-05-20 13:11:41 +0100

    Update App.jsx

--- chatify/src/App.jsx
+++ chatify/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         <Route
           path="/"
           element={
-            !isLoggedIn || !token ? <Navigate to="/login" /> : <Chattify />
+            isLoggedIn && token ? <Chattify /> : <Navigate to="/login" />
           }
         />
         <Route path="/register" element={<Register />} />
