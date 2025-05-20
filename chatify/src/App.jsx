ca888e6dfef9fdbfecf3bb46ccc86ea21ea61eef
Damian-clinton <kyleclinton54@gmail.com>
2025-05-20 19:53:14 +0100

    Update App.jsx

--- chatify/src/App.jsx
+++ chatify/src/App.jsx
@@ -4,21 +4,12 @@ import Chattify from "./components/Chattify.jsx";
 import Login from "./components/Login.jsx";
 import Register from "./components/Register.jsx";
 
-function App() {
-  const [user] = useState(() => {
-    const saved = window.localStorage.getItem("user");
-    return saved ||null;
-  });
-
-  const [token] = useState(() => window.localStorage.getItem("chat-token"));
-
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
-          path="/"
-          element={!user || !token ? <Navigate to="/login" /> : <Chattify />}
+          path="/" element={ <Chattify />}
         />
         <Route path="/register" element={<Register />} />
       </Routes>
